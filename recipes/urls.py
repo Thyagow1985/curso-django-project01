b28a484e1440881c7fb77063c0c74fda4be1ffd3
Thyago Magalhães <Thyago.tenorio@hotmail.com>
2024-01-02 12:58:00 -0300

    update

---
+++ recipes/urls.py
@@ -0,0 +1,14 @@
+# from django.contrib import admin
+from django.urls import path
+from django.http import HttpResponse
+from recipes.views import home, sobre, contato
+
+
+
+
+urlpatterns = [
+    # path('admin/', admin.site.urls),
+    path('', home),
+    path('sobre/', sobre),
+    path('contato/', contato)
+]
